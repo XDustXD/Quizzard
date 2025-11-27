fba730f9bb5ea707d595310032272d0415256345
xdustxd <xdustxd@gmail.com>
2025-11-28 02:07:23 +0500

    Добавление внешних ключей в сущности

--- Persistence/Migrations/20251124181559_AddedForeignKey.Designer.cs
+++ Persistence/Migrations/20251124181559_AddedForeignKey.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Persistence;
@@ -11,9 +12,11 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251124181559_AddedForeignKey")]
+    partial class AddedForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -94,7 +97,7 @@ namespace Persistence.Migrations
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("Description")
+                    b.Property<string>("Descripiton")
                         .IsRequired()
                         .HasColumnType("text");
 
---
+++ Persistence/Migrations/20251124181559_AddedForeignKey.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Users_UsersId",
+                table: "Results");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Results_UsersId",
+                table: "Results");
+
+            migrationBuilder.DropColumn(
+                name: "UsersId",
+                table: "Results");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Results",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuizId",
+                table: "Questions",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionId",
+                table: "Answers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_UserId",
+                table: "Results",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Users_UserId",
+                table: "Results",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Users_UserId",
+                table: "Results");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Results_UserId",
+                table: "Results");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Results");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UsersId",
+                table: "Results",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuizId",
+                table: "Questions",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionId",
+                table: "Answers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_UsersId",
+                table: "Results",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_Questions_QuestionId",
+                table: "Answers",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Users_UsersId",
+                table: "Results",
+                column: "UsersId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
